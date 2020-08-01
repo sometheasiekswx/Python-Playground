b130290ada5e1e5164e6b9c2657fe08867ffe911
Somethea Siek <sometheasiekswx@gmail.com>
2020-08-01 12:59:13 +1000

    Learn Python language features

--- advanced-python/01 Language/strings_start.py
+++ advanced-python/01 Language/strings_start.py
@@ -1,20 +1,35 @@
 # strings and bytes are not directly interchangeable
 # strings contain unicode, bytes are raw 8-bit values
 
+
 def main():
     # define some starting values
     b = bytes([0x41, 0x42, 0x43, 0x44])
     print(b)
-    
+
     s = "This is a string"
     print(s)
-    
-    # TODO: Try combining them. 
-    
+
+    # TODO: Try combining them.
+    # does not work
+    # print(b + s)
+    # works (maybe not best)
+    print(str(b) + s)
+
     # TODO: Bytes and strings need to be properly encoded and decoded
     # before you can work on them together
-    
+    b2 = b.decode('utf-8')
+    print(type(b2))
+    print(b2 + s)
+
+    s2 = s.encode('utf-8')
+    print(type(s2))
+    print(b + s2)
+
     # TODO: encode the string as UTF-32
-    
+    s3 = s.encode('utf-32')
+    print(s3)
+
+
 if __name__ == "__main__":
     main()
--- advanced-python/01 Language/templstr_start.py
+++ advanced-python/01 Language/templstr_start.py
@@ -1,18 +1,28 @@
 # demonstrate template string functions
 
+from string import Template
+
 
 def main():
     # Usual string formatting with format()
     str1 = "You're watching {0} by {1}".format("Advanced Python", "Joe Marini")
     print(str1)
-    
-    # TODO: create a template with placeholders
-    
-    # TODO: use the substitute method with keyword arguments
-    
-    # TODO: use the substitute method with a dictionary
 
-    
+    # TODO: create a template with placeholders
+    template = Template("You're watching ${title} by ${author}")
+
+    # TODO: use the substitute method with keyword arguments
+    str2 = template.substitute(title='Advanced Python', author='Joe Marini')
+    print(str2)
+
+    # TODO: use the substitute method with a dictionary
+    data = {
+        'title': 'Advanced Python',
+        'author': 'Joe Marini'
+    }
+    str3 = template.substitute(data)
+    print(str3)
+
+
 if __name__ == "__main__":
     main()
-    
